5977cb06ae94c4c766ea56b73db19c8e9e06a602
Tremayne Terry <tremaynet@gmail.com>
2020-10-13 23:58:10 -0400

    updated HTML

---
+++ assets/script.js
@@ -0,0 +1,159 @@
+/*----------------------------------------------------*/
+/* Quote Loop
+------------------------------------------------------ */
+
+function fade($ele) {
+    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function () {
+        var $next = $(this).next('.quote');
+        fade($next.length > 0 ? $next : $(this).parent().children().first());
+    });
+}
+fade($('.quoteLoop > .quote').first());
+
+
+/*----------------------------------------------------*/
+/* Navigation
+------------------------------------------------------ */
+
+$(window).scroll(function () {
+
+    if ($(window).scrollTop() > 300) {
+        $('.main_nav').addClass('sticky');
+    } else {
+        $('.main_nav').removeClass('sticky');
+    }
+});
+
+// Mobile Navigation
+$('.mobile-toggle').click(function () {
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.main_nav').removeClass('open-nav');
+    } else {
+        $('.main_nav').addClass('open-nav');
+    }
+});
+
+$('.main_nav li a').click(function () {
+    if ($('.main_nav').hasClass('open-nav')) {
+        $('.navigation').removeClass('open-nav');
+        $('.main_nav').removeClass('open-nav');
+    }
+});
+
+
+/*----------------------------------------------------*/
+/* Smooth Scrolling
+------------------------------------------------------ */
+
+jQuery(document).ready(function ($) {
+
+    $('.smoothscroll').on('click', function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+            $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 800, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+
+});
+
+
+TweenMax.staggerFrom(".heading", 0.8, { opacity: 0, y: 20, delay: 0.2 }, 0.4);
+
+
+/*----------------------------------------------------*/
+/* Portfolio Cards
+------------------------------------------------------*/
+
+Vue.config.devtools = true;
+
+Vue.component('card', {
+    template: `
+    <div class="card-wrap"
+      @mousemove="handleMouseMove"
+      @mouseenter="handleMouseEnter"
+      @mouseleave="handleMouseLeave"
+      ref="card">
+      <div class="card"
+        :style="cardStyle">
+        <div class="card-bg" :style="[cardBgTransform, cardBgImage]"></div>
+        <div class="card-info">
+          <slot name="header"></slot>
+          <slot name="content"></slot>
+        </div>
+      </div>
+    </div>`,
+    // mounted() {
+    //   this.width = this.$refs.card.offsetWidth;
+    //   this.height = this.$refs.card.offsetHeight;
+    // },
+    props: ['dataImage'],
+    data: () => ({
+        width: 0,
+        height: 0,
+        mouseX: 0,
+        mouseY: 0,
+        mouseLeaveDelay: null
+    }),
+
+    computed: {
+        mousePX() {
+            return this.mouseX / this.width;
+        },
+        mousePY() {
+            return this.mouseY / this.height;
+        },
+        cardStyle() {
+            const rX = this.mousePX * 30;
+            const rY = this.mousePY * -30;
+            return {
+                transform: `rotateY(${rX}deg) rotateX(${rY}deg)`
+            };
+
+        },
+        cardBgTransform() {
+            const tX = this.mousePX * -40;
+            const tY = this.mousePY * -40;
+            return {
+                transform: `translateX(${tX}px) translateY(${tY}px)`
+            };
+
+        },
+        cardBgImage() {
+            return {
+                backgroundImage: `url(${this.dataImage})`
+            };
+
+        }
+    },
+
+    methods: {
+        handleMouseMove(e) {
+            this.mouseX = e.pageX - this.$refs.card.offsetLeft - this.width / 2;
+            this.mouseY = e.pageY - this.$refs.card.offsetTop - this.height / 2;
+        },
+        handleMouseEnter() {
+            clearTimeout(this.mouseLeaveDelay);
+        },
+        handleMouseLeave() {
+            this.mouseLeaveDelay = setTimeout(() => {
+                this.mouseX = 0;
+                this.mouseY = 0;
+            }, 1000);
+        }
+    }
+});
+
+
+
+const app = new Vue({
+    el: '#app'
+});
+
+
+
